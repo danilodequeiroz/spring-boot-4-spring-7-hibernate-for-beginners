fa907053296943c0f5a0ba401a45addc0342d209
danilodequeiroz <danilo.aq@gmail.com>
2026-08-16 21:37:39 -0300

    Multiple implementations for Coach class.

--- 02-spring-boot-spring-core-handson/04-qualifiers-handson/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core-handson/04-qualifiers-handson/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.luv2code.springcoredemo.rest;
 
 import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class DemoController {
     private Coach coach;
 
     @Autowired
-    public void setCoach(Coach coach) {
+    public void setCoach(@Qualifier("baseballCoach") Coach coach) {
         this.coach = coach;
     }
 
